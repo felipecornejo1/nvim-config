5bfbd14eccd757a8f84144bb5824f751813df831
Felipe Cornejo <felicornejo@gmail.com>
2023-10-14 20:57:29 -0300

    chore: add navigation remap

--- lua/rulo/core/remap.lua
+++ lua/rulo/core/remap.lua
@@ -4,7 +4,10 @@ vim.g.mapleader = " "
 keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+keymap.set({ "n", "v" }, "<C-d>", "zz<C-d>zz")
+keymap.set({ "n", "v" }, "<C-u>", "zz<C-u>zz")
