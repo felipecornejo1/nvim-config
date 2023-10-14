e523a6cb27f4028655a40dc20fe20055b0780ee6
Felipe Cornejo <felicornejo@gmail.com>
2023-10-14 17:14:28 -0300

    feat: add lualine

--- lua/rulo/lazy.lua
+++ lua/rulo/lazy.lua
@@ -12,5 +12,19 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup('rulo.plugins')
+require("lazy").setup({
+	{import = 'rulo.plugins'},
+	{import = 'rulo.plugins.lsp'},
+}, {
+	install = {
+		colorscheme = { 'nightfly' },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
+})
 
