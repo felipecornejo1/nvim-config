6233190df57cec9d01cc5ba8f62b732f915622a0
Felipe Cornejo <felicornejo@gmail.com>
2023-10-18 17:27:33 -0300

    chore: add tabspaces variable

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
 require("rulo.core")
 require("rulo.lazy")
 
+local tabspaces = 4
 local set = vim.opt -- set options
-set.tabstop = 4
-set.softtabstop = 4
-set.shiftwidth = 4
+set.tabstop = tabspaces
+set.softtabstop = tabspaces
+set.shiftwidth = tabspaces
 
 set.relativenumber = true
